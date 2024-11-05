352d854a2106dd2e563357b3a718dab668782751
Max Schlichting <max.schlichting@sick.de>
2024-11-05 16:02:32 +0100

    Add tictactoe stub script

--- tic_tac_toe/my_script.js
+++ tic_tac_toe/my_script.js
@@ -1,1 +1,13 @@
 const boxes = document.querySelectorAll('td');
+
+function onClick(event) {
+    const myBox = event.target;
+    myBox.textContent = "X";
+}
+
+// boxes.forEach((box) => {})
+for (const box of boxes) {
+    box.addEventListener('click', onClick);
+}
+
+// TODO
