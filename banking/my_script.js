820b48ab44f2229e0e7f8cd51a72a09bd0049a05
Max Schlichting <max.schlichting@sick.de>
2024-11-05 10:30:46 +0100

    Add banking example

---
+++ banking/my_script.js
@@ -0,0 +1,6 @@
+const accountList = document.querySelector('ul');
+
+const loadButton = document.querySelector('#load');
+loadButton.addEventListener('click', loadClick);
+
+const url = "https://stingray-app-6557h.ondigitalocean.app";
